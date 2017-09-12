f76bbc91e13576acaa3f683f303f30b1342eeb8e
Tanner Jones <tannercolejones@gmail.com>
2017-09-11 20:21:31 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    private JPanel panelContainer;
+    private JPanel customerSearch;
+    private JPanel takeoutOption;
+    private JTextField searchField;
+    private JButton searchButton;
+    private JButton nextButton;
+    private JPanel PanelContainer;
+    private CardLayout cl = (CardLayout)panelContainer.getLayout();
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Gui");
+        frame.setContentPane(new Main().panelContainer);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+
+    public Main() {
+
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cl.show(panelContainer, "Card2");
+            }
+        });
+    }
+}
