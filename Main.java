5a83b9a7646082760254064020729a3e93762423
trinhmerced <Jane201193@gmail.com>
2017-09-25 17:34:24 -0400

    Menu code

--- Main.java
+++ Main.java
@@ -40,14 +40,21 @@ public class Main {
     private JCheckBox smallCheckBox;
     private JCheckBox olivesCheckBox;
     private JCheckBox peppersCheckBox;
-    private JButton addToCartButton;
+    private JButton addButton;
     private JButton nextButton1;
     private JLabel sizeLabel;
     private JLabel toppingLabel;
-    private JLabel drinkLabel;
-    private JCheckBox sodaCheckBox;
+    private JTextField totaltextField;
+    private JPanel Drink;
+    private JCheckBox waterCheckBox;
+    private JCheckBox cokeCheckBox;
+    private JButton add1Button;
+    private JButton nextButton2;
+    private JTextField totalField1;
+    private JCheckBox dietCokeCheckBox;
+    private JCheckBox spriteCheckBox;
     private JPanel PanelContainer;
-    private CardLayout cl = (CardLayout)panelContainer.getLayout();
+    private CardLayout cl = (CardLayout) panelContainer.getLayout();
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Gui");
@@ -71,7 +78,7 @@ public class Main {
         takeoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer,"null");
+                cl.show(panelContainer, "Menu");
             }
         });
         createButton.addActionListener(new ActionListener() {
@@ -98,5 +105,98 @@ public class Main {
 
             }
         });
+
+
+        largeCheckBox.addActionListener(actionListener);
+        mediumCheckBox.addActionListener(actionListener);
+        smallCheckBox.addActionListener(actionListener);
+        sausageCheckBox.addActionListener(actionListener);
+        pepperoniCheckBox.addActionListener(actionListener);
+        chickenCheckBox.addActionListener(actionListener);
+        peppersCheckBox.addActionListener(actionListener);
+        olivesCheckBox.addActionListener(actionListener);
+        addButton.addActionListener(actionListener);
+        cokeCheckBox.addActionListener(actionListener);
+        waterCheckBox.addActionListener(actionListener);
+        dietCokeCheckBox.addActionListener(actionListener);
+        spriteCheckBox.addActionListener(actionListener);
+        add1Button.addActionListener(actionListener);
+
+
     }
+
+    ActionListener actionListener;
+
+    {
+        actionListener = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double price;
+                double topprice;
+                double drinkprice;
+                double totalprice;
+
+                /* pizza menu */
+                price = 0;
+                if (largeCheckBox.isSelected()) {
+                    price = 16;
+                } else if (mediumCheckBox.isSelected()) {
+                    price = 14;
+                } else if (smallCheckBox.isSelected()) {
+                    price = 10;
+                }
+
+                JCheckBox[] toppings;
+                toppings = new JCheckBox[]{
+                        pepperoniCheckBox,
+                        chickenCheckBox,
+                        peppersCheckBox,
+                        sausageCheckBox,
+                        olivesCheckBox,
+
+                };
+                topprice = 0;
+                for (JCheckBox topping : toppings) {
+                    if (topping.isSelected()) {
+                        topprice += 1;
+                    }
+                }
+                if (e.getSource() == addButton) {
+                    totalprice = price + topprice;
+                    //JOptionPane.showMessageDialog(null, "total price is:" + totalprice);
+                    //System.exit(0);
+                    totaltextField.setText(Double.toString(totalprice));
+                } else {
+                    return;
+                }
+                /* Drink menu */
+                drinkprice = 0;
+                if (cokeCheckBox.isSelected()) {
+                    drinkprice += 2;
+                } else if (waterCheckBox.isSelected()) {
+                    drinkprice += 2;
+                } else if (dietCokeCheckBox.isSelected()) {
+                    drinkprice += 2;
+                } else if (spriteCheckBox.isSelected()) {
+                    drinkprice += 2.5;
+                }
+
+                if (e.getSource() == add1Button) {
+                    totalprice = price + topprice + drinkprice;
+                    //JOptionPane.showMessageDialog(null, "total price is:" + totalprice);
+                    //System.exit(0);
+                    totalField1.setText(Double.toString(totalprice));
+                }
+
+
+            }
+
+            ;
+
+
+        };
+    }
+
+    ;
 }
